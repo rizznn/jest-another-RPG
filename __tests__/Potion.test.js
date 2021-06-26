e8ab3d60a1dcabd8d33155012b5beec8a5a5ed65
Riz <80712058+rizznn@users.noreply.github.com>
2021-06-26 17:49:09 -0400

    fail, pass and refactor potion creation

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,9 @@
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion();
+//  this time, we won't check to see if the potion has the name value of health
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
